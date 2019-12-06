dc8c1e9c7d6c694be91dc7c279324723470878e6
Chad Blomquist <chad.blomquist@chadblomquistconsulting.com>
2019-12-06 10:43:46 -0800

    2015_01

---
+++ 2015/01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AoC._2015._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Input
+
+            var input = AoCUtil.GetAocInput(2015,1).First<string>();
+
+            #endregion Input
+
+            #region Problem
+
+            int floor = 0;
+            int firstBasement = -1;
+
+            for (int i = 0; i< input.Length; i++)
+            {
+                if (input[i] == ')') floor--;
+                else floor++;
+                if (floor < 0 && firstBasement == -1)
+                {
+                    firstBasement = i+1;
+                }
+            }
+
+            Console.WriteLine(floor);
+            Console.WriteLine(firstBasement);
+
+            #endregion Problem
+        }
+    }
+}
