39b4ef11a6d15f0ab1707d5b5b56ca29d51dda55
Chad Blomquist <chad.blomquist@chadblomquistconsulting.com>
2019-12-09 15:15:49 -0800

    2015_20

--- 2015/20/Program.cs
+++ 2015/20/Program.cs
@@ -11,22 +11,45 @@ namespace AoC._2015._20
         {
             #region input
 
-            int input = 29000000 / 10;
-
-            int[] houses = new int[input/2];
-
+            int input = 29000000;
 
             #endregion
 
             #region Part 1
 
+            int[] presents = new int[input / 10];
+            int house = FirstHouse(presents, input, 10);
+            Ans(string.Format("{0} presents at house#{1}", presents[house], house));
 
             #endregion Part 1
 
             #region Part 2
 
+            presents = new int[input / 10];
+            house = FirstHouse(presents, input, 11, 50);
+            Ans(string.Format("{0} presents at house#{1}", presents[house], house), 2);
 
             #endregion
+        }
+
+        static int FirstHouse(int[] presents, int target, int pm, int lazy=-1)
+        {
+            foreach (int elf in Enumerable.Range(1, presents.Length))
+            {
+                int pos = elf;
+                int pph = pm * elf;
+                int limit = lazy > 0 ? lazy : presents.Length;
+                int step = 0;
+                while (pos < presents.Length && step++ < limit) 
+                {
+                    presents[pos] += pph;
+                    pos += elf;
+                }
+            }
+            for (int i = 0; i < presents.Length; i++)
+                if (presents[i] >= target)
+                    return i;
+            return -1;
         }
     }
 }
--- 2019/10/Program.cs
+++ 2019/10/Program.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using AoC.VM;
 
 
-namespace AoC._Year._Day
+namespace AoC._2019._10
 {
     class Program : ProgramBase
     {
@@ -12,7 +11,7 @@ namespace AoC._Year._Day
         {
             #region input
             
-            var input = AoCUtil.GetAocInput(Year, Day);
+            var input = AoCUtil.GetAocInput(2019, 10);
 
             #endregion
 
