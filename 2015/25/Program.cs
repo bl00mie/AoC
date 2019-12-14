fe6d3e3e1e8fb34c08770cc993b97003e6772816
Chad Blomquist <chad.blomquist@chadblomquistconsulting.com>
2019-12-14 02:59:36 -0800

    2015_25

--- 2015/25/Program.cs
+++ 2015/25/Program.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AoC._2015._25
 {
@@ -9,20 +8,26 @@ namespace AoC._2015._25
         static void Main()
         {
             #region input
-            
-            var input = AoCUtil.GetAocInput(2015, 25);
+            Regex rx = new Regex(@" (?<row>\d+), column (?<col>\d+).");
+            var input = rx.Match(AoCUtil.GetAocInput(2015, 25).First());
+            var row = long.Parse(input.Groups["row"].Value);
+            var col = long.Parse(input.Groups["col"].Value);
 
             #endregion
 
             #region Part 1
+            long num = 1;
+            for (int i = 2; i <= row; i++)
+                num += i - 1;
+            for (int i = 1; i < col; i++)
+                num += i + row;
 
-            Ans("");
+            long val = 20151125;
+            for (int i=2; i<=num; i++)
+                val = val * 252533 % 33554393;
+
+            Ans(val);
             #endregion Part 1
-
-            #region Part 2
-
-            Ans("", 2);
-            #endregion
         }
     }
 }
