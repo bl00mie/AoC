e7f74bb4f0648290d56e955c80bf3c0d575479c0
Chad Blomquist <chad.blomquist@chadblomquistconsulting.com>
2019-12-13 23:51:04 -0800

    2019_14

---
+++ 2019/13/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AoC.VM;
+using AoC.VM.IntCode;
+
+namespace AoC._2019._13
+{
+    class Program : ProgramBase
+    {
+
+        static void Main()
+        {
+            #region input
+
+            var input = AoCUtil.GetAocInput(2019, 13).First().GetLongs().ToArray();
+            #endregion
+
+            #region Part 1
+            var vm = new VM_2019<GameIO>(input)
+            {
+                IO = new GameIO()
+            };
+
+            vm.Go();
+            Ans(vm.IO.blockCount);
+            #endregion Part 1
+
+            #region Part 2
+
+            vm.Mem[0] = 2;
+            vm.Go();
+            //Ans(vm.IO.score, 2);
+            #endregion
+        }
+    }
+
+    public class GameIO : IOContext
+    {
+        public int blockCount;
+        char[,] grid = new char[35, 21];
+        public List<long> outputs = new List<long>();
+        public long score;
+        long ballX = 0;
+        long paddleX = 0;
+        public bool play;
+
+        public long Input()
+        {
+            if (play)
+            {
+                var key = Console.ReadKey().KeyChar;
+                switch (key)
+                {
+                    case 'j': return -1;
+                    case 'l': return 1;
+                    case 'k': return 0;
+                }
+            }
+            else {
+                if (ballX > paddleX) return 1;
+                if (ballX < paddleX) return -1;
+            }
+            return 0;
+        }
+
+        public void Output(long val)
+        {
+            outputs.Add(val);
+            if (outputs.Count == 3)
+            {
+                if (outputs[0] == -1 && outputs[1] == 0) score = val;
+                else
+                {
+                    if (outputs[2] == 2) blockCount++;
+                    switch (outputs[2])
+                    {
+                        case 0:
+                            grid[outputs[0], outputs[1]] = ' ';
+                            break;
+                        case 1:
+                            grid[outputs[0], outputs[1]] = '\\';
+                            break;
+                        case 2:
+                            grid[outputs[0], outputs[1]] = '0';
+                            break;
+                        case 3:
+                            grid[outputs[0], outputs[1]] = '-';
+                            paddleX = outputs[0];
+                            break;
+                        case 4:
+                            grid[outputs[0], outputs[1]] = '*';
+                            ballX = outputs[0];
+                            break;
+                    }
+                }
+                drawGrid();
+                outputs.Clear();
+            }
+        }
+
+        void drawGrid()
+        {
+            Console.WriteLine("Score: {0}", score);
+            for (int y = 0; y<21; y++)
+            {
+                for (int x = 0; x < 35; x++)
+                    Console.Write(grid[x, y]);
+                Console.Write('\n');
+            }
+        }
+
+        public void Reset()
+        {
+            outputs.Clear();
+        }
+    }
+}
+
---
+++ 2019/14/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC._2019._14
+{
+    class Program : ProgramBase
+    {
+        static void Main()
+        {
+            #region input
+
+            var formulae = AoCUtil.GetAocInput(2019, 14).Select(L => L.Split(" => "))
+                .ToDictionary(sa => {
+                    var sa1 = sa[1].Split(' ');
+                    return sa1[1];
+                }, sa =>
+                {
+                    var sa1 = sa[1].Split(' ');
+                    return (outputCount:int.Parse(sa1[0]), requirements: sa[0].Split(", ").Select(ingredient =>
+                   {
+                       var ia = ingredient.Split(' ');
+                       return (count: int.Parse(ia[0]), name: ia[1]);
+                   }).ToList());
+                });
+
+            #endregion
+
+            #region Part 1
+
+            Dictionary<string, long> leftovers = new Dictionary<string, long>();
+
+            void reset() { leftovers = new Dictionary<string, long>(); foreach (var formula in formulae) leftovers[formula.Key] = 0; }
+            reset();
+
+            long make(string target, long needed)
+            {
+                if (leftovers[target] >= needed)
+                {
+                    leftovers[target] -= needed;
+                    return 0;
+                }
+                var formula = formulae[target];
+                long times = (long)Math.Ceiling((needed - leftovers[target]) * 1.0 / formula.outputCount);
+                leftovers[target] += times * formula.outputCount - needed;
+
+                long tot = 0;
+                foreach (var req in formula.requirements)
+                {
+                    if (req.name == "ORE") tot += times * req.count;
+                    else
+                    {
+                        tot += make(req.name, times * req.count);
+                    }
+                }
+                return tot;
+            }
+            Ans(make("FUEL", 1));
+            #endregion Part 1
+
+            #region Part 2
+
+            var lo = 0L;
+            var hi = 1000000000000L;
+            while (lo < hi)
+            {
+                reset();
+                long mid = (lo + hi) / 2;
+                long ore = make("FUEL", mid);
+                Console.Write(mid);
+                Console.Write("->");
+                Console.WriteLine(ore);
+                if (ore > 1000000000000L)
+                    hi = mid - 1;
+                else if (ore < 1000000000000)
+                    lo = mid;
+            }
+
+            Ans(lo, 2);
+            #endregion
+        }
+    }
+}
+
