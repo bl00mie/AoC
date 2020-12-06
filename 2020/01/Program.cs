4f443708eef6653ebbaf47633909beff54a5fa2a
Chad Blomquist <chad.blomquist@chadblomquistconsulting.com>
2020-12-05 22:27:59 -0800

    clean up day 1

--- 2020/01/Program.cs
+++ 2020/01/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 namespace AoC._2020._01
 {
@@ -9,44 +7,27 @@ namespace AoC._2020._01
         static void Main()
         {
             #region input
-            
-            var nums = AoCUtil.GetAocInput(2020, 01).Select(s => long.Parse(s)).ToList<long>();
-
+            var input = AoCUtil.GetAocInput(2020, 01).Select(s => long.Parse(s)).ToList<long>();
             #endregion
 
-            #region Part 1
-
-            Ans(go1(nums));
-
-            #endregion Part 1
-
-            #region Part 2
-
-            Ans(go2(nums), 2);
-            #endregion
+            bool done1 = false, done2 = false;
+            foreach (var x in input)
+                foreach (var y in input)
+                    if (x + y == 2020 && !done1)
+                    {
+                        done1 = true;
+                        Ans(x * y);
+                    }
+                    else if (x + y < 2020)
+                        foreach (var z in input)
+                            if (done2) break;
+                            else if (x + y + z == 2020)
+                            {
+                                done2 = true;
+                                Ans2(x * y * z);
+                            }
         }
-
-        static long go1(List<long> nums)
-        {
-            foreach (var x in nums)
-                foreach (var y in nums)
-                    if (x + y == 2002)
-                        return x * y;
-            return -1;
-        }
-
-        static long go2(List<long> nums)
-        {
-            foreach(var x in nums)
-                foreach(var y in nums)
-                {
-                    if (x + y > 2020) continue;
-                    foreach (var z in nums)
-                        if (x + y + z == 2020)
-                            return x * y * z;
-                }
-            return -1;
-        }
+                                
     }
 }
 
