12d56dd9bb06fc281ab13db38e093e7812259669
Chad Blomquist <cblomquist@seismic.com>
2021-12-02 23:17:02 -0800

    cleanup day3 format

--- 2021/03/Program.cs
+++ 2021/03/Program.cs
@@ -17,8 +17,7 @@ namespace AoC._2021._03
             var mul = 1;
             for (int x = input[0].Length - 1; x >= 0; x--)
             {
-                var ones = Ones(input, x).Count();
-                if (ones > input.Length / 2.0)
+                if (Ones(input, x).Count() > input.Length / 2.0)
                     gamma += mul;
                 else
                     epsilon += mul;
@@ -36,20 +35,13 @@ namespace AoC._2021._03
                 if (o2.Count() > 1)
                 {
                     var ones = Ones(o2, x);
-                    if (ones.Count() >= o2.Count() / 2.0)
-                        o2 = ones;
-                    else
-                        o2 = o2.Except(ones);
+                    o2 = ones.Count() >= o2.Count() / 2.0 ? ones : o2.Except(ones);
                 }
 
                 if (co2.Count() > 1)
                 {
                     var ones = Ones(co2, x);
-                    if (ones.Count() < co2.Count() / 2.0)
-                        co2 = ones;
-                    else
-                        co2 = co2.Except(ones);
-
+                    co2 = ones.Count() < co2.Count() / 2.0 ? ones : co2.Except(ones);
                 }
             }
 
