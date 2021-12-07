565114f9b6c369b3ec636879627764db66f847f1
Chad Blomquist <cblomquist@seismic.com>
2021-12-06 22:42:04 -0800

    tweak for concision.

--- 2021/07/Program.cs
+++ 2021/07/Program.cs
@@ -8,20 +8,17 @@ namespace AoC._2021._07
         static void Main()
         {
             var input = AoCUtil.GetAocInput(2021, 07).First().GetLongs();
-            var l = input.Min();
             var r = input.Max();
             var best = long.MaxValue;
             var best2 = long.MaxValue;
-            for (long i=l; i<r; i++)
+            for (long i=input.Min(); i<r; i++)
             {
-                var costs = input.Select(x => Math.Abs(x - i));
-                var tot = costs.Sum();
-                if (tot < best)
-                    best = tot;
+                var distances = input.Select(x => Math.Abs(x - i));
+                var tot = distances.Sum();
+                best = Math.Min(tot, best);
 
-                var tot2 = costs.Select(x => x * (x + 1) / 2).Sum();
-                if (tot2 < best2)
-                    best2 = tot2;
+                var tot2 = distances.Select(x => x * (x + 1) / 2).Sum();
+                best2 = Math.Min(best2, tot2);
             }
             Ans(best);
             Ans2(best2);
