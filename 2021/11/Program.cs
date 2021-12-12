d8430a9b8b539028de972bd0fdc12e816e481b29
Chad Blomquist <cblomquist@seismic.com>
2021-12-12 00:16:29 -0800

    2021 day 11 and 12

---
+++ 2021/11/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC._2021._11
+{
+    class Program : ProgramBase
+    {
+        static void Main()
+        {
+            #region input
+            #region Stopwatch 
+            Stopwatch.Start();
+            #endregion
+
+            var grid = new Grid<int>(AoCUtil.GetAocInput(2021, 11)
+                .Select(line => line.Select(c => (int)(c-'0'))));
+            
+            #region Stopwatch
+            Stopwatch.Stop();
+            WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
+            Stopwatch.Restart();
+            #endregion
+            #endregion
+
+            long total = 0;
+            for (int i = 0; true; i++)
+            {
+                var flashers = new List<Point>();
+                foreach (var (p, v) in grid)
+                    if (++grid[p] > 9)
+                        flashers.Add(p);
+                while (flashers.Any())
+                {
+                    foreach(var flasher in flashers)
+                    {
+                        var neighbors = grid.Neighbors(flasher, Grid.AllDirs);
+                        foreach (var (p,_) in neighbors)
+                            grid[p]++;
+                        grid[flasher] = int.MinValue;
+                    }
+                    total += flashers.Count;
+                    flashers.Clear();
+                    foreach (var (p, v) in grid)
+                        if (v > 9)
+                            flashers.Add(p);
+                }
+                if (i == 99)
+                    Ans(total);
+
+                if (!grid.Where(x => x.v >= 0).Any())
+                {
+                    Ans2(i + 1);
+                    break;
+                }
+
+                foreach (var (p, v) in grid)
+                    if (v < 0)
+                        grid[p] = 0;
+            }
+        }
+    }
+}
+
---
+++ 2021/12/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RegExtract;
+
+namespace AoC._2021._12
+{
+    class Program : ProgramBase
+    {
+        static void Main()
+        {
+            #region input
+            #region Stopwatch 
+            Stopwatch.Start();
+            #endregion
+
+            var input = AoCUtil.GetAocInput(2021, 12).Extract<(string, string)>(@"(\w+)-(\w+)");
+            var paths = new Dictionary<string, List<string>>();
+            foreach (var pair in input)
+            {
+                if (!paths.ContainsKey(pair.Item1)) paths.Add(pair.Item1, new List<string>());
+                if (!paths.ContainsKey(pair.Item2)) paths.Add(pair.Item2, new List<string>());
+                paths[pair.Item2].Add(pair.Item1);
+                paths[pair.Item1].Add(pair.Item2);
+            }
+            
+            #region Stopwatch
+            Stopwatch.Stop();
+            WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
+            Stopwatch.Restart();
+            #endregion
+            #endregion
+
+            #region Part 1
+            int success = 0;
+            var attempts = new List<List<string>> { new List<string>(new[] { "start" }) };
+            while(attempts.Any())
+            {
+                var attempt = attempts[^1];
+                attempts.RemoveAt(attempts.Count-1);
+                if (attempt[^1] == "end")
+                {
+                    success++;
+                    continue;
+                }
+                foreach (var allowed in paths[attempt[^1]].Where(path => char.IsUpper(path[0]) || !attempt.Contains(path)))
+                {
+                    var newAttempt = new List<string>(attempt);
+                    newAttempt.Add(allowed);
+                    attempts.Add(newAttempt);
+                }
+            }
+            Ans(success);
+            #endregion Part 1
+
+            #region Part 2
+            success = 0;
+            attempts = new List<List<string>> { new List<string>(new[] { "start" }) };
+            while (attempts.Any())
+            {
+                var attempt = attempts[^1];
+                attempts.RemoveAt(attempts.Count - 1);
+                if (attempt[^1] == "end")
+                {
+                    success++;
+                    continue;
+                }
+                foreach (var path in paths[attempt[^1]])
+                {
+                    if (path == "start") continue;
+                    if (char.IsLower(path[0]))
+                        if (attempt.Contains(path))
+                        {
+                            if (attempt.Where(s => char.IsLower(s[0])).GroupBy(s => s).Where(group => group.Count() == 2).Any())
+                                continue;
+                        }
+                    var newAttempt = new List<string>(attempt);
+                    newAttempt.Add(path);
+                    attempts.Add(newAttempt);
+                }
+            }
+            Ans2(success);
+            #endregion
+        }
+    }
+}
+
