714f31df77a8d1382b29c475dbad2fd5dd8da5bd
Chad Blomquist <cblomquist@seismic.com>
2022-12-02 13:55:22 -0800

    2022 day 1 and 2

---
+++ 2022/1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RegExtract;
+
+namespace AoC._2022._1
+{
+    class Program : ProgramBase
+    {
+        static void Main()
+        {
+            #region input
+            #region Stopwatch 
+            Stopwatch.Start();
+            #endregion
+
+            var elves = AoCUtil.GroupInput(2022, 1).Select(g => g.Sum(l => int.Parse(l)));
+            
+            #region Stopwatch
+            Stopwatch.Stop();
+            WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
+            Stopwatch.Restart();
+            #endregion
+            #endregion
+
+            #region Part 1
+
+            var max = int.MinValue;
+            foreach (var cal in elves)
+            {
+                if (cal > max) max = cal;
+            }
+
+            Ans(max);
+            #endregion Part 1
+
+            #region Part 2
+            
+            var sorted = elves.OrderByDescending(x => x).ToList();
+            Ans(sorted[0]+sorted[1]+sorted[2], 2);
+            
+            #endregion
+        }
+    }
+}
+
---
+++ 2022/2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using RegExtract;
+
+namespace AoC._2022._2
+{
+    class Program : ProgramBase
+    {
+        static void Main()
+        {
+            #region input
+            #region Stopwatch 
+            Stopwatch.Start();
+            #endregion
+
+            var input = AoCUtil.GetAocInput(2022, 2).Extract<(char l, char r)>(@"(\w) (\w)").ToList();
+            
+            #region Stopwatch
+            Stopwatch.Stop();
+            WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
+            Stopwatch.Restart();
+            #endregion
+            #endregion
+
+            #region Part 1
+            var ans1 = 0;
+            foreach (var (l,r) in input)
+            {
+                var a = l - 'A';
+                var b = r - 'X';
+                ans1 += ((b - a + 3) % 3) switch
+                {
+                    1 => 6,
+                    0 => 3,
+                    _ => 0
+                } + (b + 1);
+            }
+            Ans(ans1);
+            #endregion Part 1
+
+            #region Part 2
+            var ans2 = 0;
+            foreach (var (l, r) in input)
+            {
+                var a = l - 'A';
+                ans2 += r switch
+                {
+                    'X' => (a + 2) % 3 + 1,
+                    'Y' => a + 4,
+                    'Z' => (a + 1) % 3 + 7
+                };
+            }
+            Ans(ans2, 2);
+            #endregion
+        }
+    }
+}
+
