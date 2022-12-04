2e0ab076ce27b2edd4e8bc65e367148a052ad62e
Chad Blomquist <cblomquist@seismic.com>
2022-12-03 21:37:05 -0800

    add template projects for all 25 days

---
+++ 2022/9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RegExtract;
+
+namespace AoC._2022._9
+{
+    class Program : ProgramBase
+    {
+        static void Main()
+        {
+            #region input
+            #region Stopwatch 
+            Stopwatch.Start();
+            #endregion
+
+            var input = AoCUtil.GetAocInput(2022, 9);
+            
+            #region Stopwatch
+            Stopwatch.Stop();
+            WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
+            Stopwatch.Restart();
+            #endregion
+            #endregion
+            
+            #region Part 1
+
+            Ans("");
+            #endregion Part 1
+
+            #region Part 2
+
+            //Ans("", 2);
+            #endregion
+        }
+    }
+}
+
