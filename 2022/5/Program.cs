639dfedb3bc0129dae44bc500dc7609fdc433837
Chad Blomquist <cblomquist@seismic.com>
2022-12-04 23:09:03 -0800

    2022 day 5

--- 2022/5/Program.cs
+++ 2022/5/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using AoCUtil;
 using RegExtract;
 
 namespace AoC._2022._5
@@ -14,24 +15,42 @@ namespace AoC._2022._5
             Stopwatch.Start();
             #endregion
 
-            var input = AoCUtil.GetAocInput(2022, 5);
-            
+            var inputGroups = AoCUtil.GroupInput(2022, 5).ToArray();
+            var top = inputGroups[0].ToArray();
+            var moves = inputGroups[1].Extract<(int count, int from, int to)>(@"move (\d+) from (\d+) to (\d+)");
+
             #region Stopwatch
             Stopwatch.Stop();
             WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
             Stopwatch.Restart();
             #endregion
             #endregion
-            
-            #region Part 1
 
-            Ans("");
+            #region Part 1
+            var crates = ParseCrates(top);
+            foreach (var (count, from, to) in moves)
+                crates[to-1].Push(crates[from-1].Pop(count));
+            
+            Ans(new string(crates.Select(col => col.Peek()).ToArray()));
             #endregion Part 1
 
             #region Part 2
+            crates = ParseCrates(top);
+            foreach (var (count, from, to) in moves)
+                crates[to-1].Push(crates[from-1].Pop(count).Reverse());
 
-            //Ans("", 2);
+            Ans2(new string(crates.Select(col=>col.Peek()).ToArray()));
             #endregion
+        }
+
+        static List<Stack<char>> ParseCrates(string[] input)
+        {
+            var crates = new List<Stack<char>> { new(), new(), new(), new(), new(), new(), new(), new(), new()};
+            for (int i = 0; i < 9; i++)
+                for (int j = input.Length - 2; j >= 0; j--)
+                    if (input[j][4 * i + 1] != ' ') 
+                       crates[i].Push(input[j][4 * i + 1]);
+            return crates;
         }
     }
 }
---
+++ AoCUtil/CollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AoCUtil
+{
+    public static class CollectionExtensions
+    {
+        public static IEnumerable<T> Pop<T>(this Stack<T> stack, int count)
+        {
+            foreach (var _ in Enumerable.Range(0, count))
+                yield return stack.Pop();
+        }
+
+        public static void Push<T>(this Stack<T> stack, IEnumerable<T> items)
+        {
+            foreach (var item in items)
+                stack.Push(item);
+        }
+    }
+}
