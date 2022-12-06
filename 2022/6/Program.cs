90fe43d67e440bc3b19cae6d5f5c512b8671e51e
Chad Blomquist <cblomquist@seismic.com>
2022-12-05 23:07:31 -0800

    22,6

--- 2022/6/Program.cs
+++ 2022/6/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using RegExtract;
+﻿using System.Linq;
 
 namespace AoC._2022._6
 {
@@ -13,25 +10,30 @@ namespace AoC._2022._6
             #region Stopwatch 
             Stopwatch.Start();
             #endregion
-
-            var input = AoCUtil.GetAocInput(2022, 6);
-            
+            var input = AoCUtil.GetAocInput(2022, 6).Single();
             #region Stopwatch
             Stopwatch.Stop();
             WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
             Stopwatch.Restart();
             #endregion
             #endregion
-            
-            #region Part 1
 
-            Ans("");
+            #region Part 1
+            Ans(FindSequence(input));
             #endregion Part 1
 
             #region Part 2
-
-            //Ans("", 2);
+            Ans2(FindSequence(input, 14));
             #endregion
+        }
+
+        static int FindSequence(string input, int size=4)
+        {
+            var ans = size;
+            while (true)
+                if (input[(ans - size)..ans++].ToHashSet().Count == size)
+                    break;
+            return ans - 1;
         }
     }
 }
