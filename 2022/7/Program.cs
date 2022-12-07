ccbbdc15e62faf03f322d05d1fbd4e789f747cc2
Chad Blomquist <cblomquist@seismic.com>
2022-12-06 23:36:35 -0800

    22-7

--- 2022/7/Program.cs
+++ 2022/7/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using RegExtract;
 
 namespace AoC._2022._7
 {
@@ -13,24 +11,34 @@ namespace AoC._2022._7
             #region Stopwatch 
             Stopwatch.Start();
             #endregion
-
-            var input = AoCUtil.GetAocInput(2022, 7);
-            
+            var input = AoCUtil.GetAocInput(2022, 7).Select(line => line.Split(' '));
             #region Stopwatch
             Stopwatch.Stop();
             WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
             Stopwatch.Restart();
             #endregion
             #endregion
-            
-            #region Part 1
 
-            Ans("");
+            #region Part 1
+            var dirs = new AoCDictionary<string, int>(0, true);
+            var path = new List<string>();
+            foreach (var cmd in input)
+                if (cmd[1] == "ls" || cmd[0] == "dir") continue;
+                else if (cmd[1] == "cd")
+                    if (cmd[2][0] == '.') path.RemoveAt(path.Count - 1);
+                    else path.Add(cmd[2]);
+                else
+                {
+                    int size = int.Parse(cmd[0]);
+                    for (int i = 1; i <= path.Count; i++)
+                        dirs[string.Join('/', path.GetRange(0,i))] += size;
+                }
+            Ans(dirs.Values.Where(size => size <= 100_000).Sum());
             #endregion Part 1
 
             #region Part 2
-
-            //Ans("", 2);
+            var neededSpace = dirs["/"] - 40_000_000;
+            Ans2(dirs.Values.Where(sz => sz >= neededSpace).Min());
             #endregion
         }
     }
