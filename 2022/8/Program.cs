0a0b89954f51d3fa6d0d76f4b121ec061b16215f
Chad Blomquist <cblomquist@seismic.com>
2022-12-07 22:31:05 -0800

    2022-08

--- 2022/8/Program.cs
+++ 2022/8/Program.cs
@@ -13,24 +13,63 @@ namespace AoC._2022._8
             #region Stopwatch 
             Stopwatch.Start();
             #endregion
-
-            var input = AoCUtil.GetAocInput(2022, 8);
-            
+            var f = new Grid<char>(AoCUtil.GetAocInput(2022, 8));
             #region Stopwatch
             Stopwatch.Stop();
             WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
             Stopwatch.Restart();
             #endregion
             #endregion
-            
-            #region Part 1
 
-            Ans("");
+            #region Part 1
+            var ans = f.W*2 + (f.H-2)*2;
+            foreach (var (p, h) in f)
+            {
+                if (f.IsEdge(p)) continue;
+                foreach (var v in GridVector.ESWN)
+                {
+                    var n = p + v;
+                    var vis = true;
+                    while (f.ContainsCoord(n))
+                    {
+                        if (f[n] >= h)
+                        {
+                            vis = false;
+                            break;
+                        }
+                        n.x += v.dx; n.y += v.dy;
+                    }
+                    if (vis)
+                    {
+                        ans++;
+                        break;
+                    }
+                }
+            }
+            Ans(ans);
             #endregion Part 1
 
             #region Part 2
-
-            //Ans("", 2);
+            var ans2 = int.MinValue;
+            foreach (var (p, h) in f)
+            {
+                if (f.IsEdge(p)) continue;
+                var score = 1;
+                foreach (var v in GridVector.ESWN)
+                {
+                    var n = p + v;
+                    var vscore = 1;
+                    while(f[n]<h)
+                    {
+                        n += v;
+                        if (!f.ContainsCoord(n)) break;
+                        vscore++;
+                    } 
+                    score *= vscore;
+                }
+                ans2 = Math.Max(ans2, score);
+            }
+            Ans2(ans2);
             #endregion
         }
     }
