fadc186b2bcc04f1624fe5ad22b2d1dbd4e7faae
Chad Blomquist <cblomquist@seismic.com>
2022-12-10 23:21:17 -0800

    2022 10 & 11

--- 2022/10/Program.cs
+++ 2022/10/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using RegExtract;
 
 namespace AoC._2022._10
 {
@@ -15,23 +12,49 @@ namespace AoC._2022._10
             #endregion
 
             var input = AoCUtil.GetAocInput(2022, 10);
-            
+
             #region Stopwatch
             Stopwatch.Stop();
             WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
             Stopwatch.Restart();
             #endregion
             #endregion
-            
+
             #region Part 1
 
-            Ans("");
+            var grid = new string[6] { string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty };
+            var X = 1;
+            var cycles = 0;
+            var signal = 0L;
+            foreach (var line in input)
+            {
+                switch (line[..4])
+                {
+                    case "addx":
+                        signal += CheckSignal(++cycles, X, grid);
+                        signal += CheckSignal(++cycles, X, grid);
+                        X += int.Parse(line[5..]);
+                        break;
+                    case "noop":
+                        signal += CheckSignal(++cycles, X, grid);
+                        break;
+                }
+            }
+            Ans(signal);
             #endregion Part 1
 
             #region Part 2
 
-            //Ans("", 2);
+            foreach (var line in grid)
+                WL(line);
             #endregion
+        }
+
+        static int CheckSignal(int cycles, int X, string[] g)
+        {
+            var p = cycles - 1;
+            g[p / 40] += Math.Abs(X - (p % 40)) <= 1 ? '#' : ' ';
+            return cycles % 40 == 20 ? cycles * X : 0;
         }
     }
 }
--- 2022/11/Program.cs
+++ 2022/11/Program.cs
@@ -9,29 +9,81 @@ namespace AoC._2022._11
     {
         static void Main()
         {
-            #region input
-            #region Stopwatch 
-            Stopwatch.Start();
-            #endregion
+            var input = AoCUtil.GroupInput(2022, 11);
 
-            var input = AoCUtil.GetAocInput(2022, 11);
-            
-            #region Stopwatch
-            Stopwatch.Stop();
-            WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
-            Stopwatch.Restart();
-            #endregion
-            #endregion
-            
-            #region Part 1
+            var items = new List<List<long>>();
+            var items2 = new List<List<long>>();
+            var inspections = new List<int>();
+            var inspections2 = new List<int>();
+            var operations = new List<(int a, int b, bool plus)>();
+            var tests = new List<(int div, int mt, int mf)>();
+            long mod = 1;
 
-            Ans("");
-            #endregion Part 1
+            foreach (var group in input)
+            {
+                var monkey = group.ToList();
+                
+                items.Add(monkey[1][18..].Split(", ").Select(s => long.Parse(s)).ToList());
+                items2.Add(items[^1].ToList());
+                
+                var opStrs = monkey[2][19..].Split(' ');
+                operations.Add((
+                    opStrs[0][0] == 'o' ? -1 : int.Parse(opStrs[0]),
+                    opStrs[2][0] == 'o' ? -1 : int.Parse(opStrs[2]),
+                    opStrs[1][0] == '+'));;
 
-            #region Part 2
+                var div = int.Parse(monkey[3][monkey[3].LastIndexOf(' ')..]);
+                mod *= div;
+                tests.Add((div, monkey[4][^1] - '0', monkey[5][^1] - '0'));
 
-            //Ans("", 2);
-            #endregion
+                inspections.Add(0);
+                inspections2.Add(0);
+            }
+
+
+            foreach (int i in Enumerable.Range(0, 10000))
+            {
+                for (int id = 0; id < items.Count; id++)
+                {
+                    var (a, b, plus) = operations[id];
+                    var (div, mt, mf) = tests[id];
+
+                    if (i < 20)
+                    {
+                        foreach (var val in items[id])
+                        {
+                            var worry = Operation(val, a, b, plus) / 3;
+                            items[worry % div == 0 ? mt : mf].Add(worry);
+                            inspections[id]++;
+                        }
+                        items[id].Clear();
+                    }
+
+                    foreach (var val in items2[id])
+                    {
+                        var worry = Operation(val, a, b, plus) % mod;
+                        items2[worry % div == 0 ? mt : mf].Add(worry);
+                        inspections2[id]++;
+                    }
+                    items2[id].Clear();
+                }
+                
+                if (i == 20)
+                {
+                    inspections.Sort();
+                    Ans(inspections[^2] * inspections[^1]);
+                }
+            }
+
+            inspections2.Sort();
+            Ans2(1L * inspections2[^2] * inspections2[^1]);
+        }
+
+        static long Operation(long val, long a, long b, bool plus)
+        {
+            a = a < 0 ? val : a;
+            b = b < 0 ? val : b;
+            return plus ? a + b : a * b;
         }
     }
 }
