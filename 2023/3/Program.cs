77f34e5eb73de29abcfa8c17ab0f3bfb803caea6
Chad Blomquist <chad.blomquist@gmail.com>
2023-12-03 22:34:15 -0800

    days 3 and 4

--- 2023/3/Program.cs
+++ 2023/3/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using RegExtract;
 
 namespace AoC._2023._3
 {
@@ -14,23 +13,77 @@ namespace AoC._2023._3
             Stopwatch.Start();
             #endregion
 
-            var input = AoCUtil.GetAocInput(2023, 3);
-            
+            var input = AoCUtil.GetAocInput(2023, 3).ToList();
+            var engine = new Grid<char>(input);
+
             #region Stopwatch
             Stopwatch.Stop();
             WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
             Stopwatch.Restart();
             #endregion
             #endregion
-            
-            #region Part 1
 
-            Ans("");
+            #region Part 1
+            var ans = 0;
+            var ans2 = 0;
+            var nums = new HashSet<List<(Coord p, char v)>>();
+            for (int y = 0; y < input.Count; y++)
+            {
+                var num = new List<(Coord p, char v)>();
+                for (int x = 0; x < input[0].Length; x++)
+                {
+                    if (char.IsDigit(engine[x, y]))
+                    {
+                        num.Add((new Coord(x, y), engine[x, y]));
+                        if (x+1 == input[0].Length)
+                        {
+                            nums.Add(num);
+                            num = new();
+                        }
+                    }
+                    else if (num.Count != 0)
+                    {
+                        nums.Add(num);
+                        num = new();
+                    }
+                }    
+            }
+
+            var gears = new Dictionary<Coord, List<int>>();
+            foreach (var num in nums)
+            {
+                foreach ((Coord p, char v) in num)
+                {
+                    var nb = engine.Neighbors(p, GridVector.DirsAll, ((Coord p, char v) mine, (Coord p, char v) theirs) => !char.IsDigit(theirs.v) && theirs.v != '.');
+                    if (nb.Any())
+                    {
+                        var val = num[0].v - '0';
+                        for (int i = 1; i < num.Count; i++)
+                        {
+                            val *= 10;
+                            val += num[i].v - '0';
+                        }
+                        ans += val;
+                        var sym = nb.First();
+                        if (sym.v == '*')
+                        {
+                            if (!gears.ContainsKey(sym.p))
+                                gears[sym.p] = new();
+                            gears[sym.p].Add(val);
+                        }
+                        break;
+                    }
+                }
+            }
+            var twos = gears.Values.Where(nums => nums.Count == 2);
+            ans2 = twos.Sum(l => l.Aggregate((a, b) => a * b));
+
+            Ans(ans);
             #endregion Part 1
 
             #region Part 2
 
-            //Ans("", 2);
+            Ans2(ans2);
             #endregion
         }
     }
--- 2023/4/Program.cs
+++ 2023/4/Program.cs
@@ -14,23 +14,56 @@ namespace AoC._2023._4
             Stopwatch.Start();
             #endregion
 
-            var input = AoCUtil.GetAocInput(2023, 4);
-            
+            var input = AoCUtil.GetAocInput(2023, 4)
+                .Extract<(int cn, string ws, string ms)>(@"Card ([0-9 ]+)\: ([0-9 ]+)\|([0-9 ]+)")
+                .Select(x =>
+                {
+                    var w = x.ws.Split(" ").Where(s => !string.IsNullOrWhiteSpace(s)).Select(int.Parse).ToHashSet<int>();
+                    var m = x.ms.Split(" ").Where(s => !string.IsNullOrWhiteSpace(s)).Select(int.Parse).ToHashSet<int>();
+                    return (x.cn, w, m);
+                })
+                .ToList();
             #region Stopwatch
             Stopwatch.Stop();
             WL($"Input processed in {Stopwatch.ElapsedMilliseconds} ms");
             Stopwatch.Restart();
             #endregion
             #endregion
-            
-            #region Part 1
 
-            Ans("");
+            #region Part 1
+            var ans = 0;
+            var matches = new Dictionary<int, int>();
+            var copies = new AoCDictionary<int, int>();
+            foreach (var (cn, w, m) in input)
+            {
+                var match = w.Intersect(m).Count();
+                if (match > 0)
+                    ans += (int)Math.Pow(2, match-1);
+                matches[cn] = match;
+                for(int i=1; i<=match; i++)
+                    copies[cn + i]++;
+            }
+            
+            Ans(ans);
             #endregion Part 1
 
             #region Part 2
-
-            //Ans("", 2);
+            var ans2 = input.Count;
+            while (copies.Count > 0)
+            {
+                ans2 += copies.Sum(x => x.Value);
+                var next = new AoCDictionary<int, int>();
+                foreach(var x in copies)
+                {
+                    var num = x.Value;
+                    var cn = x.Key;
+                    var match = matches[cn];
+                    for (int i = 1; i <= match; i++)
+                        next[cn + i]+=num;
+                }
+                copies = next;
+            }
+            Ans2(ans2);
             #endregion
         }
     }
