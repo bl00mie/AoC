8a869b7029f6947a8b5cc44e63f301b7d994932d
Chad Blomquist <chad.blomquist@gmail.com>
2023-12-09 23:26:07 -0800

    day 10

--- 2023/10/Program.cs
+++ 2023/10/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using RegExtract;
 
 namespace AoC._2023._10
 {
@@ -14,7 +14,9 @@ namespace AoC._2023._10
             Stopwatch.Start();
             #endregion
 
-            var input = AoCUtil.GetAocInput(2023, 10);
+            var input = AoCUtil.GetAocInput(2023, 10)
+                .SelectMany((l, y) => l.Select((c, x) => (new Coord(x,y), c)))
+                .ToDictionary(x => x.Item1, x => x.c);
             
             #region Stopwatch
             Stopwatch.Stop();
@@ -22,15 +24,56 @@ namespace AoC._2023._10
             Stopwatch.Restart();
             #endregion
             #endregion
-            
-            #region Part 1
 
-            Ans("");
+            #region Part 1
+            var S = input.Where(p => p.Value == 'S').Single();
+
+            (Coord a, Coord b) Exits(Coord p) => input[p] switch
+            {
+                '|' => (p + GridVector.N, p + GridVector.S),
+                '-' => (p + GridVector.E, p + GridVector.W),
+                'L' => (p + GridVector.E, p + GridVector.S),
+                'F' => (p + GridVector.N, p + GridVector.E),
+                '7' => (p + GridVector.W, p + GridVector.N),
+                'J' => (p + GridVector.S, p + GridVector.W),
+                _ => throw new Exception("fuuu")
+            };
+            var FL = new List<Coord>();
+            foreach (var v in GridVector.DirsESNW)
+            {
+                var (a, b) = Exits(S.Key + v);
+                if (a == S.Key || b == S.Key) FL.Add(S.Key + v);
+            }
+
+            var path = new HashSet<Coord>() { S.Key, FL[0] };
+            while (FL[0] != FL[1])
+            {
+                var (a, b) = Exits(FL[0]);
+                FL[0] = path.Contains(b) ? a : b;
+                path.Add(FL[0]);
+            }
+
+            Ans(path.Count / 2 + path.Count % 2);
             #endregion Part 1
 
             #region Part 2
+            var crosses = new HashSet<char>() { '-', 'J', '7' };
+            foreach (var p in input.Where(kv => !path.Contains(kv.Key)))
+                input[p.Key] = '.';
+            foreach (var p in input.Where(kv => kv.Value == '.'))
+            {
+                var trace = p.Key;
+                var cross = 0;
+                while (trace.y != 0)
+                {
+                    if (crosses.Contains(input[trace]))
+                        cross++;
+                    trace += GridVector.S;
+                }
+                input[p.Key] = cross % 2 == 0 ? 'O' : 'I';
+            }
 
-            //Ans("", 2);
+            Ans(input.Count(p => p.Value == 'I'));
             #endregion
         }
     }
